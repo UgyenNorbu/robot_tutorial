25b3109385c5ccd428469db2a64f0cd786b4139c
Ugyen Norbu <unorbu2019@gmail.com>
2020-07-13 08:52:18 +0600

    Multiple turtles with random work.

---
+++ multiple_turtles.py
@@ -0,0 +1,29 @@
+import turtle
+import random as rn
+
+t_1 = turtle.Turtle()
+t_2 = turtle.Turtle()
+t_3 = turtle.Turtle()
+t_4 = turtle.Turtle()
+
+t_1.speed(15)
+t_2.speed(15)
+t_3.speed(15)
+t_4.speed(15)
+
+t_1.pencolor("#F1C40F")
+t_2.pencolor("#5DADE2")
+t_3.pencolor("#E74C3C")
+t_4.pencolor("#239B56")
+
+for i in range(100):
+    t_1.right(rn.randint(0, 360))
+    t_1.forward(rn.randrange(10, 50, 2))
+    t_2.right(rn.randint(0, 360))
+    t_2.forward(rn.randrange(10, 50, 3))
+    t_3.right(rn.randint(0, 360))
+    t_3.forward(rn.randrange(10, 50, 5))
+    t_4.right(rn.randint(0,360))
+    t_4.forward(rn.randrange(10, 50, 6))
+
+turtle.exitonclick()
