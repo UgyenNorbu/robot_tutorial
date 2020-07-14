e3992f92566312227c3a80ee94c2c4ba2063f785
Ugyen Norbu <unorbu2019@gmail.com>
2020-07-14 23:21:48 +0600

    game
    Game source code compiled by Jigsel

--- math_fn_art.py
+++ math_fn_art.py
@@ -6,7 +6,7 @@ t_1.speed(25)
 t_1.pencolor("#0E934A")
 
 for i in range(500):
-    t_1.forward(math.sin(i)*45)
-    t_1.right(i%12)
+    t_1.forward(math.sin(i)*10)
+    t_1.right(math.sqrt(i*9))
 
 turtle.exitonclick()
