bc392cda2571c7f8a943bd1b5e3027b2b1ebdf92
Ugyen Norbu <unorbu2019@gmail.com>
2020-07-19 11:18:20 +0600

    code

--- math_fn_art.py
+++ math_fn_art.py
@@ -6,7 +6,9 @@ t_1.speed(25)
 t_1.pencolor("#0E934A")
 
 for i in range(500):
-    t_1.forward(math.sin(i)*10)
-    t_1.right(math.sqrt(i*9))
+    t_1.forward((i)^2)
+    t_1.right(135)
+
 
 turtle.exitonclick()
+# turtle.done()
